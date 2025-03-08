d624d37a1f83f754c21f534314b271e5713afb19
Eduard Bolma <edaurd.bolma@gmail.com>
2025-03-08 14:42:54 +0100

    Task 1 is completed

--- taks_1.py
+++ taks_1.py
@@ -1,25 +1,36 @@
 
-# Створіть функцію get_days_from_today(date), яка розраховує кількість днів між заданою датою і поточною датою.
+'''
+    Створіть функцію get_days_from_today(date), яка розраховує кількість днів між заданою датою і поточною датою.
 
-# Вимоги до завдання:
-# Функція приймає один параметр: date — рядок, що представляє дату у форматі 'РРРР-ММ-ДД' (наприклад, '2020-10-09').
-# Функція повертає ціле число, яке вказує на кількість днів від заданої дати до поточної. Якщо задана дата пізніша за поточну, результат має бути від'ємним.
-# У розрахунках необхідно враховувати лише дні, ігноруючи час (години, хвилини, секунди).
-# Для роботи з датами слід використовувати модуль datetime Python.
-
-
-# Рекомендації для виконання:
-# Імпортуйте модуль datetime.
-# Перетворіть рядок дати у форматі 'РРРР-ММ-ДД' у об'єкт datetime.
-# Отримайте поточну дату, використовуючи datetime.today().
-# Розрахуйте різницю між поточною датою та заданою датою.
-# Поверніть різницю у днях як ціле число.
+    Вимоги до завдання:
+    Функція приймає один параметр: date — рядок, що представляє дату у форматі 'РРРР-ММ-ДД' (наприклад, '2020-10-09').
+    Функція повертає ціле число, яке вказує на кількість днів від заданої дати до поточної. Якщо задана дата пізніша за поточну, результат має бути від'ємним.
+    У розрахунках необхідно враховувати лише дні, ігноруючи час (години, хвилини, секунди).
+    Для роботи з датами слід використовувати модуль datetime Python.
+'''
 
 import datetime as dt
 from datetime import datetime as dtdt
 
-def get_days_from_today(date):
-    now = dtdt.now()
-    print(now)
+def get_days_from_today(date: str) -> int:
+    '''
+        Function calculates the number of days between current date and provided date
+        
+        :param date: String in 'yyyy-mm-dd' format
+        :return: Integer with number of days  between current date and provided date
+            Returns -1 if there's an error (e.g., invalid date format).
+    '''
     
-get_days_from_today('2024-04-11')
+    date_format = "%Y-%m-%d"
+    
+    try: 
+        f_date = dtdt.strptime(date, date_format)
+    except ValueError:
+        print('Format of the date is not correct. The format should be "yyyy-mm-dd"')
+        return -1
+    
+    current_date = dtdt.today()
+    
+    diff = current_date - f_date
+
+    return diff.days
